aca02a168a992cf15f965d90aa279ba95562920f
Luca Rosellini <luca.rosellini@gmail.com>
2015-02-12 17:24:15 +0100

    added sendThroughput JMX metric

--- src/main/java/org/apache/flume/sink/kafka/KafkaSinkCounter.java
+++ src/main/java/org/apache/flume/sink/kafka/KafkaSinkCounter.java
@@ -2,53 +2,65 @@ package org.apache.flume.sink.kafka;
 
 import org.apache.flume.instrumentation.MonitoredCounterGroup;
 
-public class KafkaSinkCounter extends MonitoredCounterGroup implements KafkaSinkCounterMBean{
+public class KafkaSinkCounter extends MonitoredCounterGroup implements KafkaSinkCounterMBean {
 
-	private static long counter_message_sent;
-	private static long counter_message_sent_error;
-	private static long last_sent;
-	private static long start_time;
-	
-	
-	private static final String[] ATTRIBUTES = {
-		"counter_message_sent","counter_message_sent_error","last_sent","start_time"
-	  };
-	
-	public KafkaSinkCounter(String name) {
-		super(MonitoredCounterGroup.Type.SINK, name, ATTRIBUTES);
-		counter_message_sent = 0;
-		counter_message_sent_error = 0;
-		last_sent = 0;
-		setStartTime();
-	}
-	
-	public long increaseCounterMessageSent(){
-		last_sent = System.currentTimeMillis();
-		return counter_message_sent++;
-	}
-	
-	public long getCounterMessageSent(){
-		return counter_message_sent;
-	}
+    private static long counter_message_sent;
+    private static long counter_message_sent_error;
+    private static long last_sent;
+    private static long start_time;
+    private static long sendThroughput;
 
-	public long getLastSent() {
-		return last_sent;
-	}
-	
-	public long setStartTime(){
-		start_time = System.currentTimeMillis();
-		return start_time;
-	}
-	
-	public long getStartTime(){
-		return start_time;
-	}
+    private static final String[] ATTRIBUTES = {
+            "counter_message_sent", "counter_message_sent_error", "last_sent", "start_time"
+    };
 
-	public long increaseCounterMessageSentError() {
-		return counter_message_sent_error++;
-	}
+    public KafkaSinkCounter(String name) {
+        super(MonitoredCounterGroup.Type.SINK, name, ATTRIBUTES);
+        counter_message_sent = 0;
+        counter_message_sent_error = 0;
+        last_sent = 0;
+        sendThroughput = 0;
+        setStartTime();
+    }
 
-	public long getCounterMessageSentError() {
-		return counter_message_sent_error;
-	}
+    public long increaseCounterMessageSent() {
+        last_sent = System.currentTimeMillis();
+        counter_message_sent++;
+
+        if (last_sent > start_time) {
+            sendThroughput = counter_message_sent / ((last_sent - start_time) / 1000);
+        }
+        return counter_message_sent;
+    }
+
+    public long getCounterMessageSent() {
+        return counter_message_sent;
+    }
+
+    public long getLastSent() {
+        return last_sent;
+    }
+
+    public long setStartTime() {
+        start_time = System.currentTimeMillis();
+        return start_time;
+    }
+
+    public long getStartTime() {
+        return start_time;
+    }
+
+    public long increaseCounterMessageSentError() {
+        return counter_message_sent_error++;
+    }
+
+    public long getCounterMessageSentError() {
+        return counter_message_sent_error;
+    }
+
+    @Override
+    public long getSendThroughput() {
+        return sendThroughput;
+    }
+
 }
